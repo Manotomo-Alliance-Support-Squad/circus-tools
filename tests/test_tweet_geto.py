97029e049eec404d7eda736d47d5b4ff46590d14
Zapple Cat <zizibaluba@gmail.com>
2021-06-07 04:49:23 -0500

    Empty tests with TODOs

---
+++ tests/test_tweet_geto.py
@@ -0,0 +1,12 @@
+from unittest.mock import MagicMock, patch
+
+
+# TODO: Patch TwitterPager to figure out if query is sent as we expect
+# TODO: Patch TwitterOAuth to figure out if filepath is dealt with the correct way
+# TODO: Patch TwitterAPI to figure out if the right auth informaion is pass through
+def test_get_recent_search_pager():
+    pass
+
+
+def dump_pager_content_to_json():
+    pass

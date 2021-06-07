a00f0187d3f71225de55c16a59295bfcbfae6b0b
Zapple Cat <zizibaluba@gmail.com>
2021-06-07 04:48:58 -0500

    Tools for getting tweets and dumping them as json

---
+++ tools/tweet_geto.py
@@ -0,0 +1,75 @@
+"""Convinience functions for getting tweets
+"""
+
+import json
+from pathlib import Path
+from typing import Dict
+
+from TwitterAPI import HydrateType, TwitterAPI, TwitterOAuth, TwitterPager, OAuthType
+
+EXPANSIONS = 'author_id,attachments.media_keys'
+TWEET_FIELDS = 'created_at,attachments,author_id'
+MEDIA_FIELDS = 'media_key,preview_image_url,type,url'
+
+
+aqua_query = {
+    # TODO: Add in a query limit for datetime
+    'query': {'#Ganbareあくたん -is:retweet'},
+    'expansions': 'author_id,attachments.media_keys',
+    'tweet.fields': 'created_at,attachments,author_id',
+    'media.fields': 'media_key,preview_image_url,type,url'
+}
+
+
+def get_recent_search_pager(
+    query: str,
+    fields: Dict,
+    auth_filepath: Path = None,
+    hydrate_type: HydrateType = HydrateType.APPEND
+) -> TwitterPager:
+    # When auth_filepath is None, it looks for a credentials.txt in the dir of the current file
+    auth = TwitterOAuth(auth_filepath.resolve() if auth_filepath.is_file else None)
+    api = TwitterAPI(auth.consumer_key, auth.consumer_secret, auth_type=OAuthType.OAUTH2, api_version='2')
+
+    api_request = dict(**fields, **{"query": query})
+    return TwitterPager(
+        api,
+        'tweets/search/recent',
+        api_request,
+        hydrate_type=HydrateType.APPEND
+    )
+
+
+def dump_pager_content_to_json(pager: TwitterPager, filepath: Path):
+    if filepath.suffix != ".json":
+        raise ValueError("Filetype should be a json file.")
+
+    all_pager_results = [result for result in pager.get_iterator()]
+
+    with open(filepath.resolve(), 'w') as fp:
+        json.dump(all_pager_results, fp)
+
+
+def append_pager_content_to_json(
+    pager: TwitterPager, filepath: Path, remove_dups: bool = False, no_bak_file: bool = False
+):
+    """Appends a pagers content to an already existing file. Note that remove_dups will remove any dups.
+
+    In case of any unintended file changes, a .bak of the original file is dropped into the location the file is
+    read from.
+    """
+    with open(filepath, 'r') as fp:
+        current_results = json.load(fp)
+
+    if not no_bak_file:
+        bak_name = filepath.name + ".bak"
+        filepath.rename(filepath.parent.joinpath(bak_name))
+
+    for result in pager.get_iterator():
+        current_results.append(result)
+
+    if remove_dups:
+        list(set(current_results))
+
+    with open(filepath, 'w') as fp:
+        json.dump(current_results, fp)

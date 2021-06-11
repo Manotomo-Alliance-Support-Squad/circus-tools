d483413254b2f5f9fce52206b7e6a4c9420aae5b
Zapple Cat <zizibaluba@gmail.com>
2021-06-11 00:44:34 -0500

    Create TweetContext class

--- tools/tweet_transform.py
+++ tools/tweet_transform.py
@@ -47,7 +47,62 @@ HEADER_MAP = {
 }
 
 
-def build_tweet_context(tweet: Dict) -> Dict:
+class TweetContext(object):
+    """docstring for TweetContext"""
+
+    def __init__(self, context_dict: Dict):
+        self.context = context_dict
+
+    #  Tweet-vocab getters
+    def get_tweet_id(self) -> str:
+        return self.context.get("tweet_id")
+
+    def get_tweet_name(self) -> str:
+        return self.context.get("display_name")
+
+    def get_tweet_media(self) -> List:
+        """There could be more than one piece of media"""
+        media_urls = []
+        for media in self.context.get("medias"):
+            if "preview_image_url" in media:
+                media_urls.append(media["preview_image_url"])
+            elif "url" in media:
+                media_urls.append(media["url"])
+            else:
+                LOGGER.warning(f"Media did not have preview_image_url or url in its keys. Media: {pformat(media)}")
+
+        return media_urls
+
+    def get_tweet_text(self) -> str:
+        return self.context.get("text")
+
+    def get_tweet_url(self) -> str:
+        return f"https://twitter.com/{self.context.get('username')}/status/{self.context.get('tweet_id')}"
+
+    # Manotomo-vocab getters
+    @property
+    def username(self) -> str:
+        return self.get_tweet_name()
+
+    @property
+    def artworkLinks(self) -> List:
+        """There could be more than one piece of media"""
+        return self.get_tweet_media()
+
+    @property
+    def artistLink(self) -> str:
+        return self.get_tweet_url()
+
+    @property
+    def setID(self) -> str:
+        return self.get_tweet_id()
+
+    @property
+    def message(self) -> str:
+        return self.get_tweet_text()
+
+
+def build_tweet_context(tweet: Dict) -> TweetContext:
     """Takes a tweet format and transforms the context into how we map our DBs.
     """
     current_tweet_context = {}

4b2b84e1d0f8c4c550b129b224af3bdf77779704
Zapple Cat <zizibaluba@gmail.com>
2021-07-08 00:33:50 -0500

    Splitting up tooling into different branches for lighter PR

--- tools/tweet_geto.py
+++ tools/tweet_geto.py
@@ -13,16 +13,10 @@ MEDIA_FIELDS = 'media_key,preview_image_url,type,url'
 
 RECENT_TWEETS_ENDPOINT = 'tweets/search/recent'
 
-aqua_query = {
-    # TODO: Add in a query limit for datetime
-    'query': {'#Ganbareあくたん -is:retweet'},
-    'expansions': EXPANSIONS,
-    'tweet.fields': TWEET_FIELDS,
-    'media.fields': MEDIA_FIELDS,
-}
-
 
 def get_api_obj_with_auth(auth_filepath: Path = None):
+    """Gets a TwitterAPI object using Twitter developer credentials.
+    """
     # When auth_filepath is None, it looks for a credentials.txt in the dir of the current file
     if auth_filepath is not None and auth_filepath.is_file():
         auth = TwitterOAuth.read_file(auth_filepath.resolve())
@@ -38,6 +32,17 @@ def get_recent_search_pager(
     auth_filepath: Path = None,
     hydrate_type: HydrateType = HydrateType.APPEND
 ) -> TwitterPager:
+    """Gets an TwitterAPI object and hits the recent tweets endpoint to retrive a TwitterPager object.
+
+    Example api_request
+    aqua_api_request = {
+        'query': {'#Ganbareあくたん -is:retweet'},
+        'expansions': EXPANSIONS,
+        'tweet.fields': TWEET_FIELDS,
+        'media.fields': MEDIA_FIELDS,
+    }
+
+    """
     api = get_api_obj_with_auth(auth_filepath)
 
     if not isinstance(query, str):
@@ -53,6 +58,8 @@ def get_recent_search_pager(
 
 
 def dump_pager_content_to_json(pager: TwitterPager, filepath: Path):
+    """Dumps a TwitterPager's content into a JSON file
+    """
     if filepath.suffix != '.json':
         raise ValueError('Filetype should be a json file.')
 
@@ -60,30 +67,3 @@ def dump_pager_content_to_json(pager: TwitterPager, filepath: Path):
 
     with open(filepath.resolve(), 'w') as fp:
         json.dump(all_pager_results, fp)
-
-
-# FIXME: This doesn't work as intended
-def append_pager_content_to_json(
-    pager: TwitterPager, filepath: Path, remove_dups: bool = False, no_bak_file: bool = False
-):
-    """Appends a pagers content to an already existing file. Note that remove_dups will remove any dups.
-
-    In case of any unintended file changes, a .bak of the original file is dropped into the location the file is
-    read from.
-    """
-    with open(filepath, 'r') as fp:
-        current_results = json.load(fp)
-
-    if not no_bak_file:
-        bak_name = filepath.name + '.bak'
-        filepath.rename(filepath.parent.joinpath(bak_name))
-
-    for result in pager.get_iterator():
-        current_results.append(result)
-
-    # FIXME: doesn't work since dicts are in JSONs and are unhashable
-    if remove_dups:
-        list(set(current_results))
-
-    with open(filepath, 'w') as fp:
-        json.dump(current_results, fp)
